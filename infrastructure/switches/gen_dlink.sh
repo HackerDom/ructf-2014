ba44f885a53e7a06c7eecf2e1614fba98e43d6ef
Andrey Malets <malets@yandex-team.ru>
2014-04-05 23:40:36 +0600

    added "clean" parameter to D-Link router config generator

--- infrastructure/switches/gen_dlink.sh
+++ infrastructure/switches/gen_dlink.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 N=${1?no team count}
+[ -n "$2" ] && clean=true || clean=false
 cat <<END
 config ipif system ipaddress 10.24.0.1/24 vlan default 
 END
 
-echo "delete vlan JURY"
+$clean || echo "delete vlan JURY"
 echo "create vlan JURY tag 100"
 echo "config vlan JURY add untagged 1:(23)"
 echo "config vlan JURY add tagged 1:(24)"
 
-echo "delete vlan EXTERNAL"
+$clean || echo "delete vlan EXTERNAL"
 echo "create vlan EXTERNAL tag 300"
 echo "config vlan EXTERNAL add untagged 1:(21-22)"
 echo "config vlan EXTERNAL add tagged 1:(24)"
 
 for i in $(seq 1 $N); do
-    echo "delete vlan TEAM$i"
+    $clean || echo "delete vlan TEAM$i"
     echo "create vlan TEAM$i tag $((100+i))"
     echo "config vlan TEAM$i add tagged 1:($i)"
     echo "config vlan TEAM$i add tagged 1:(24)"
 done
 
 for i in $(seq 1 $N); do
-    echo "delete vlan RTEAM$i"
+    $clean || echo "delete vlan RTEAM$i"
     echo "create vlan RTEAM$i tag $((200+i))"
     echo "config vlan RTEAM$i add untagged 1:($i)"
     echo "config vlan RTEAM$i add tagged 1:(24)"
