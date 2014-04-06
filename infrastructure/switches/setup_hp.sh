4fc5a9ff8413b24ada90367729e55b3c7641b8fc
Andrey Malets <andrey.malets@gmail.com>
2014-04-06 14:04:00 -0400

    refactoring stage 2: add do_setup for convenience

--- infrastructure/switches/setup_hp.sh
+++ infrastructure/switches/setup_hp.sh
@@ -24,47 +24,41 @@ check_ping() {
     return 1
 }
 
-configure() {
-    if ! $SETUP reset $DEFAULT_IP ""; then
-        echo "Failed to reset $DEFAULT_IP" >&2
-        return 2
+do_setup() {
+    local action="$1" ip=$2
+    if ! $SETUP "$@"; then
+        echo "Failed to do $action on $ip" >&2
+        return 1
     fi
+}
+
+configure() {
+    do_setup reset $DEFAULT_IP "" || return 2
     check_ping $DEFAULT_IP 30 reset || return 3
 
     NORMAL_IP=10.24.$N.1
     NORMAL_MASK=$DEFAULT_MASK
     NORMAL_GW=10.24.$N.254
-    if ! $SETUP change_ip $DEFAULT_IP "" \
-            $NORMAL_IP $NORMAL_MASK $NORMAL_GW; then
-        echo "Failed to change ip to $NORMAL_IP" >&2
-        return 4
-    fi
-
+    do_setup change_ip $DEFAULT_IP "" \
+        $NORMAL_IP $NORMAL_MASK $NORMAL_GW || return 4
     check_ping $NORMAL_IP 5 'address change' || return 5
 
     pwd=$(grep "^$N:" $(dirname $0)/hp_pwds | cut -f2 -d:)
-    if ! $SETUP change_pwd $NORMAL_IP "" "$pwd"; then
-        echo "Can't change pwd on $NORMAL_IP" >&2
-        return 6
-    fi
+    do_setup change_pwd $NORMAL_IP "" "$pwd" || return 6
     check_ping $NORMAL_IP 5 'password change' || return 7
 
     TEAM_VLAN=$((100+N))
-    if ! $SETUP add_vlan $NORMAL_IP "$pwd" $TEAM_VLAN "$(seq -s, 1 24)"; then
-        echo "Can't configure VLAN #$TEAM_VLAN on $NORMAL_IP" >&2
-        return 8
-    fi
+    do_setup add_vlan $NORMAL_IP \
+        "$pwd" $TEAM_VLAN "$(seq -s, 1 24)" || return 8
     check_ping $NORMAL_IP 5 'VLAN configuration' || return 9
 
     vlan_aware=23,24
     ingress_filter=$(seq -s, 1 22)
     tagged=
     pvids=$(for i in {1..22}; do echo -n ${TEAM_VLAN},; done; echo -n 1,1)
-    if ! $SETUP configure_ports $NORMAL_IP "$pwd" \
-            "$vlan_aware" "$ingress_filter" "$tagged" "$pvids"; then
-        echo "Can't configure port VLANs on $NORMAL_IP" >&2
-        return 10
-    fi
+    do_setup configure_ports $NORMAL_IP "$pwd" \
+        "$vlan_aware" "$ingress_filter" "$tagged" "$pvids" || return 10
+    check_ping $NORMAL_IP 5 'port configuration' || return 11
 }
 
 ip addr add $DEFAULT_GW/$DEFAULT_PREFIX dev $DEV
