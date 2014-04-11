dba542faf9a9ef5042df923f8887eaaf3edb99bb
Andrey Malets <andrey.malets@gmail.com>
2014-04-11 16:47:13 +0600

    Fixed MAC addresses for game images

--- infrastructure/router/gen_dhcp.py
+++ infrastructure/router/gen_dhcp.py
@@ -3,26 +3,9 @@
 import jinja2, sys
 
 if __name__ == '__main__':
-    N=sys.argv[1]
-    #TODO(uzer): write cubies macs here
-    cubies = [
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-        '00:00:00:00:00:00',
-    ]
+    N=int(sys.argv[1])
+    vulnimage = 'c0:b0:11:22:33:44'
+    testimage = 'c0:b0:11:22:33:00'
 
     template="""
 option domain-name-servers 8.8.8.8;
@@ -31,24 +14,26 @@ default-lease-time 600;
 ddns-update-style none;
 {% for i in range(1,N+1) %}
 subnet 10.23.{{i}}.0 netmask 255.255.255.0 {
-    range 10.23.{{i}}.10 10.23.{{i}}.253;
+    range 10.23.{{i}}.10 10.23.{{i}}.254;
     option routers 10.23.{{i}}.1;
 }
 {% endfor %}
 {% for i in range(1,N+1) %}
-host test{{ loop.index }} {
+host testimage{{ loop.index }} {
     fixed-address 10.23.{{ loop.index }}.2;
-    hardware ethernet 00:11:22:33:44:55;
+    hardware ethernet {{ testimage }};
     option host-name test{{ loop.index }};
 }
 {% endfor %}
-{% for mac in cubies %}
-host cubie{{ loop.index }} {
+{% for i in range(1,N+1) %}
+host vulnimage{{ loop.index }} {
     fixed-address 10.23.{{ loop.index }}.3;
-    hardware ethernet {{ mac }};
+    hardware ethernet {{ vulnimage }};
     option host-name team{{ loop.index }};
 }
 {% endfor %}
     """
 
-    print jinja2.Template(template).render(cubies=cubies, N=int(N))
+    print jinja2.Template(template).render(vulnimage=vulnimage,
+                                           testimage=testimage,
+                                           N=N)
