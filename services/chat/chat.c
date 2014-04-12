afc77d170dd2134987049d04d57be6d98de8f58e
dimmoborgir <dimmoborgir@gmail.com>
2014-04-13 01:29:14 +0600

    1. no zombie processes 2. little cleanup

--- services/chat/chat.c
+++ services/chat/chat.c
@@ -19,8 +19,6 @@
 
 #define die(message) { perror(message); exit(1); }
 
-FILE* new_stream;
-
 int create_server_socket (int port)
 {
 	int sock;
@@ -47,23 +45,117 @@ int create_server_socket (int port)
 	return sock;
 }
 
-int main(int argc, char ** argv)
+void process_client(client)
 {
 	char buf[MAX_MSG_LENGTH] = {0};
 
+	printf("Hello, Client!\r\n");
+	while(1) {
+		short int readc = 0, filled = 0;
+		while(1) {
+			char UName[MAX_MSG_LENGTH]={0};
+			send(client, "Login: ", 8, 0);
+			readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
+			buf[readc-2]=0;
+			strcpy(UName,buf);
+			D(UName);
+						
+			if(readc <= 0) break;
+			if(strcmp(buf,"root")==0)
+			{
+				int i=0;
+				
+				strcpy(UName,buf);
+				while(1)
+				{
+					int login=1;
+					char chOriginalPassword[]="root";
+					send(client, "Password: ",10, 0);
+					readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
+					readc-=2;
+					D("\t[%d]=%s\n", readc,buf);
+					buf[readc]=0;	
+					if(readc<1)break;
+					readc++;
+					for(i=0;i<readc;i++)
+					{
+						if(buf[i]!=chOriginalPassword[i])
+						{
+							login=0;
+						}
+					}
+					if(login)
+					{
+						D(UName);
+						char msg[MAX_MSG_LENGTH]="Hi, ";
+						strcat(msg,UName);
+						send(client, msg,strlen(msg), 0);
+						strcpy(msg,"\nexit - Exit\nsend-Send\ninRoom-spisok komnat\n");
+						send(client, msg,strlen(msg), 0);
+						while(1)
+						{
+							readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
+							if(readc<1)break;
+							buf[readc-2]=0;
+							if(readc <= 0) break;
+							if(strcmp(buf,"inRoom")==0)
+							{
+								strcpy(msg,"spisok komnat:\n");
+								send(client, msg,strlen(msg), 0);
+								strcpy(msg,"1");
+								send(client, msg,strlen(msg), 0);
+								while(1)
+								{
+
+									readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
+									if(readc<1)break;
+									buf[readc-2]=0;
+									if(readc <= 0) break;
+										if(strcmp(buf,"1")==0){
+											strcpy(msg,"Send:");
+											send(client, msg,strlen(msg), 0);
+											while(1)
+											{
+												readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
+												if(readc<1)break;
+												buf[readc-2]=0;
+												if(readc <= 0) break;
+											}}
+									
+								}
+							}else 	if(strcmp(buf,"exit")==0)
+							{
+								close(client);
+								exit(0);
+							}
+						}
+					}else{
+						send(client, "NO pass",10, 0);
+					}
+				}
+			}
+		}
+		if(!readc) {
+			D("\t[] Client disconnected.\n");
+			break;
+		}
+	}
+	close(client);
+}
+
+int main(int argc, char ** argv)
+{
 	int port = argc >= 2 ? atoi(argv[1]) : DEFAULT_PORT;
-	int sock = create_server_socket(port);
+	int server = create_server_socket(port);
 
 	struct sockaddr cli_addr;
 	int cli_len = sizeof(cli_addr);
 	int client, new_fd, pid;
 
-	// if(new_fd = dup(STDERR_FILENO) == -1) die("dup");
-	// new_stream = fdopen(new_fd, "w");
-	// setbuf(new_stream, NULL); // sin buffering
+	signal(SIGCHLD, SIG_IGN);
 
 	while (1) {
-		client = accept(sock, &cli_addr, &cli_len);
+		client = accept(server, &cli_addr, &cli_len);
 		if (client < 0)
 			die("accept");
 
@@ -71,111 +163,15 @@ int main(int argc, char ** argv)
 		if (pid < 0)
 			die("fork");
 
-		if (pid > 0) {
-			D("New client connected. PID: %d\n", pid);
-		}
-		else {
-			pid = getpid();
-			if(client < 0) die("accept");
-			if(dup2(client, STDOUT_FILENO) == -1) die("dup2");
-			printf("Hello, Client!\r\n");
-			while(1) {
-				short int readc = 0, filled = 0;
-				while(1) {
-					char UName[MAX_MSG_LENGTH]={0};
-					send(client, "Login: ", 8, 0);
-					readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
-					buf[readc-2]=0;
-					strcpy(UName,buf);
-					D(UName);
-								
-					if(readc <= 0) break;
-					if(strcmp(buf,"root")==0)
-					{
-						int i=0;
-						
-						strcpy(UName,buf);
-						while(1)
-						{
-							int login=1;
-							char chOriginalPassword[]="root";
-							send(client, "Password: ",10, 0);
-							readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
-							readc-=2;
-							D("\t[%d]=%s\n", readc,buf);
-							buf[readc]=0;	
-							if(readc<1)break;
-							readc++;
-							for(i=0;i<readc;i++)
-							{
-								if(buf[i]!=chOriginalPassword[i])
-								{
-									login=0;
-								}
-							}
-							if(login)
-							{
-								D(UName);
-								char msg[MAX_MSG_LENGTH]="Hi, ";
-								strcat(msg,UName);
-								send(client, msg,strlen(msg), 0);
-								strcpy(msg,"\nexit - Exit\nsend-Send\ninRoom-spisok komnat\n");
-								send(client, msg,strlen(msg), 0);
-								while(1)
-								{
-									readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
-									if(readc<1)break;
-									buf[readc-2]=0;
-									if(readc <= 0) break;
-									if(strcmp(buf,"inRoom")==0)
-									{
-										strcpy(msg,"spisok komnat:\n");
-										send(client, msg,strlen(msg), 0);
-										strcpy(msg,"1");
-										send(client, msg,strlen(msg), 0);
-										while(1)
-										{
-
-											readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
-											if(readc<1)break;
-											buf[readc-2]=0;
-											if(readc <= 0) break;
-												if(strcmp(buf,"1")==0){
-													strcpy(msg,"Send:");
-													send(client, msg,strlen(msg), 0);
-													while(1)
-													{
-														readc = recv(client, buf,  MAX_MSG_LENGTH, 0);
-														if(readc<1)break;
-														buf[readc-2]=0;
-														if(readc <= 0) break;
-													}}
-											
-										}
-									}else 	if(strcmp(buf,"exit")==0)
-									{
-										close(client);
-										D("\t[%d] Dying.", pid);
-										exit(0);
-									}
-								}
-							}else{
-								send(client, "NO pass",10, 0);
-							}
-						}
-					}
-				}
-				if(!readc) {
-					D("\t[%d] Client disconnected.\n", pid);
-					break;
-				}
-			}
-			close(client);
-			D("\t[%d] Dying.", pid);
+		if (pid == 0) {
+			if(dup2(client, STDOUT_FILENO) < 0) 
+				die("dup2");
+			D("  [%d] process started\n", getpid());
+			process_client(client);
+			D("  [%d] process finished\n", getpid());
 			exit(0);
 		}
 	}
-	fclose(new_stream);
-	close(sock);
+	close(server);
 	return 0;
 }
