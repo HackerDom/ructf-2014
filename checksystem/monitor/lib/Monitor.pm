113785174c05918b8869dabe0a8b8af74a500121
Andrey Khozov <avkhozov@gmail.com>
2014-04-13 22:02:00 +0600

    Now using async driver for Pg

--- checksystem/monitor/lib/Monitor.pm
+++ checksystem/monitor/lib/Monitor.pm
@@ -1,9 +1,10 @@
 package Monitor;
 use Mojo::Base 'Mojolicious';
 
-use DBI;
-
-has cache => sub { +{} };
+has services   => sub { [] };
+has teams      => sub { {} };
+has scoreboard => sub { [] };
+has round      => sub { {} };
 
 sub startup {
   my $self = shift;
@@ -11,17 +12,98 @@ sub startup {
   my $mode = $self->mode;
   $self->plugin('Config', file => "monitor.$mode.conf");
 
+  $self->plugin(
+    'PgAsync',
+    dbi => [
+      $self->config->{db}{source}, $self->config->{db}{user},
+      $self->config->{db}{pass}, {AutoCommit => 1, RaiseError => 1, pg_enable_utf8 => 1}]);
+
   my $r = $self->routes;
   $r->get('/')->to('main#index');
   $r->get('/flags')->to('main#flags');
 
-  $self->helper(
-    db => sub {
-      DBI->connect_cached(
-        $self->config->{db}->{source},
-        $self->config->{db}->{user},
-        $self->config->{db}->{pass},
-        {RaiseError => 1, AutoCommit => 1, pg_enable_utf8 => 1});
+  $self->pg(
+    'SELECT name FROM services;',
+    sub {
+      my $db = shift;
+
+      $self->log->info('Fetch services at startup');
+      while (my $row = $db->sth->fetchrow_hashref()) {
+        push @{$self->services}, $row->{name};
+      }
+    });
+
+  $self->pg(
+    'SELECT id, name, vuln_box FROM teams;',
+    sub {
+      my $db = shift;
+
+      $self->log->info('Fetch teams at startup');
+      while (my $row = $db->sth->fetchrow_hashref()) {
+        $self->teams->{$row->{id}} = $row;
+      }
+    });
+
+  Mojo::IOLoop->recurring(
+    3 => sub {
+      $self->log->info('Update scoreboard');
+      Mojo::IOLoop->delay(
+        sub {
+          my $delay = shift;
+
+          $self->log->info('Fetch SLA and FP for all team');
+          $self->pg(
+            'SELECT DISTINCT ON (team) team AS team_id, score
+            FROM score ORDER BY team, time DESC;'
+              => $delay->begin
+          );
+          $self->pg(
+            'SELECT DISTINCT ON (team_id) team_id, successed, failed
+            FROM sla ORDER BY team_id, time DESC;'
+              => $delay->begin
+          );
+          $self->pg(
+            'SELECT n, EXTRACT(EPOCH FROM time) AS time
+            FROM rounds ORDER BY n DESC LIMIT 1;'
+              => $delay->begin
+          );
+        },
+        sub {
+          my ($delay, $fh, $sh, $rh) = @_;
+          my ($flag_points, $sla_points);
+
+          while (my $row = $sh->sth->fetchrow_hashref()) {
+            my ($sla, $sum) = (0, $row->{successed} + $row->{failed});
+            $sla = $row->{successed} / $sum if $sum > 0;
+            $sla_points->{$row->{team_id}} = $sla;
+          }
+          while (my $row = $fh->sth->fetchrow_hashref) {
+            $flag_points->{$row->{team_id}} = $row->{score};
+          }
+
+          my @data;
+          for my $tid (keys %{$self->teams}) {
+            my $team = $self->teams->{$tid};
+            my $fp   = $flag_points->{$tid} // 0;
+            my $sla  = $sla_points->{$tid} // 0;
+
+            push @data,
+              {
+              team  => {id => $tid, name => $team->{name}, vuln_box => $team->{vuln_box}},
+              sla   => $sla,
+              fp    => $fp,
+              score => $fp * $sla
+              };
+          }
+
+          @data = sort { $b->{score} <=> $a->{score} } @data;
+          $self->scoreboard(\@data);
+
+          # TODO: save @data to file
+
+          my $row = $rh->sth->fetchrow_hashref;
+          $self->round({n => $row->{n}, time => scalar localtime int $row->{time}});
+        });
     });
 }
 
--- checksystem/monitor/lib/Monitor/Main.pm
+++ checksystem/monitor/lib/Monitor/Main.pm
@@ -4,33 +4,7 @@ use Mojo::Base 'Mojolicious::Controller';
 sub index {
   my $self = shift;
 
-  my $db = $self->db;
-
-  my $flag_points = $self->_flag_points;
-  my $sla_points  = $self->_sla_points;
-
-  my @teams;
-  my $teams = $self->_teams;
-  for my $tid (keys %$teams) {
-    my $team = $teams->{$tid};
-    my $fp   = $flag_points->{$tid} // 0;
-    my $sla  = $sla_points->{$tid} // 0;
-
-    push @teams,
-      {
-      tid     => $tid,
-      name    => $team->{name},
-      vuln_box => $team->{vuln_box},
-      fp      => $fp,
-      sla     => $sla,
-      score   => $fp * $sla
-      };
-  }
-  @teams = sort { $b->{score} <=> $a->{score} } @teams;
-
-  $self->stash(teams    => \@teams);
-  $self->stash(services => $self->_service_status);
-  $self->stash(round    => $self->_round);
+  # $self->stash(services => $self->_service_status);
   $self->render;
 }
 
@@ -42,87 +16,6 @@ sub flags {
   $self->stash(teams    => $self->_teams);
   $self->stash(round    => $self->_round);
   $self->render;
-}
-
-sub _flag_points {
-  my $self = shift;
-  my $flag_points;
-  my $db = $self->db;
-
-  my $fp = $self->app->cache->{fp};
-  if ($fp && $fp->{expires} > time) {
-    $flag_points = $fp->{data};
-  } else {
-    my $stm = $db->prepare(
-      q{
-      SELECT DISTINCT ON (team) team AS team_id, score
-      FROM score ORDER BY team, time DESC
-    }
-    );
-    $stm->execute() or $self->app->log->warn("SQL error [$DBI::err]: $DBI::errstr");
-    while (my $row = $stm->fetchrow_hashref()) {
-      $flag_points->{$row->{team_id}} = $row->{score};
-    }
-
-    my $cache = $self->app->cache;
-    $cache->{fp} = {expires => 60 + time, data => $flag_points};
-    $self->app->cache($cache);
-  }
-
-  return $flag_points;
-}
-
-sub _sla_points {
-  my $self = shift;
-  my $sla_points;
-  my $db = $self->db;
-
-  my $sla = $self->app->cache->{sla};
-  if ($sla && $sla->{expires} > time) {
-    $sla_points = $sla->{data};
-  } else {
-    my $stm = $db->prepare(
-      q{
-      SELECT DISTINCT ON (team_id) team_id, successed, failed
-      FROM sla ORDER BY team_id, time DESC;
-    }
-    );
-    $stm->execute() or $self->app->log->warn("SQL error [$DBI::err]: $DBI::errstr");
-    while (my $row = $stm->fetchrow_hashref()) {
-      my ($sla, $sum) = (0, $row->{successed} + $row->{failed});
-      $sla = $row->{successed} / $sum if $sum > 0;
-      $sla_points->{$row->{team_id}} = $sla;
-    }
-
-    my $cache = $self->app->cache;
-    $cache->{sla} = {expires => 60 + time, data => $sla_points};
-    $self->app->cache($cache);
-  }
-
-  return $sla_points;
-}
-
-sub _teams {
-  my $self = shift;
-  my $teams;
-  my $db = $self->db;
-
-  my $t = $self->app->cache->{t};
-  if ($t && $t->{expires} > time) {
-    $teams = $t->{data};
-  } else {
-    my $stm = $db->prepare('SELECT id, name, vuln_box FROM teams;');
-    $stm->execute() or $self->app->log->warn("SQL error [$DBI::err]: $DBI::errstr");
-    while (my $row = $stm->fetchrow_hashref()) {
-      $teams->{$row->{id}} = $row;
-    }
-
-    my $cache = $self->app->cache;
-    $cache->{t} = {expires => 60 * 10 + time, data => $teams};
-    $self->app->cache($cache);
-  }
-
-  return $teams;
 }
 
 sub _service_status {
@@ -169,51 +62,6 @@ sub _flags {
   }
 
   return $flags;
-}
-
-sub _services {
-  my $self = shift;
-  my $services;
-  my $db = $self->db;
-
-  my $s = $self->app->cache->{s};
-  if ($s && $s->{expires} > time) {
-    $services = $s->{data};
-  } else {
-    my $stm = $db->prepare('SELECT name FROM services;');
-    $stm->execute() or $self->app->log->warn("SQL error [$DBI::err]: $DBI::errstr");
-    while (my $row = $stm->fetchrow_hashref()) {
-      push @$services, $row->{name};
-    }
-
-    my $cache = $self->app->cache;
-    $cache->{s} = {expires => 60 * 10 + time, data => $services};
-    $self->app->cache($cache);
-  }
-
-  return $services;
-}
-
-sub _round {
-  my $self = shift;
-  my $round;
-  my $db = $self->db;
-
-  my $r = $self->app->cache->{r};
-  if ($r && $r->{expires} > time) {
-    $round = $r->{data};
-  } else {
-    my $stm = $db->prepare('SELECT n, EXTRACT(EPOCH FROM time) AS time FROM rounds ORDER BY n DESC LIMIT 1');
-    $stm->execute() or $self->app->log->warn("SQL error [$DBI::err]: $DBI::errstr");
-    my $row = $stm->fetchrow_hashref();
-    $round = {n => $row->{n}, time => scalar gmtime int $row->{time}};
-
-    my $cache = $self->app->cache;
-    $cache->{r} = {expires => 20 + time, data => $round};
-    $self->app->cache($cache);
-  }
-
-  return $round;
 }
 
 1;
