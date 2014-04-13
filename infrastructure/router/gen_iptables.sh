722e641df92ad26d46d2a3aedabcb279f97025bd
Andrey Malets <andrey.malets@gmail.com>
2014-04-13 14:58:35 +0600

    generate forwarding rules in iptables

--- infrastructure/router/gen_iptables.sh
+++ infrastructure/router/gen_iptables.sh
@@ -1,47 +1,74 @@
 #!/bin/bash
 
 N=${1?no teams count}
+inet_ifaces=${2? no inet ifaces}
+team2team=$([ -n "$3" ] && echo true || echo false)
+
+vpn=172.16.19/24
+
+core_switch=10.24/24
+team_switches=10.24/19
+
+checksystem=10.23/24
+devs=10.23.$N/24
+teams=10.23/19
+
+any=0/0
 
 iptables -P FORWARD ACCEPT
 
 init_chain() {
     local table=$1 chain=$2
-    ipt="iptables -t $table"
-    $ipt -L $chain &>/dev/null && \
-        $ipt -F $chain || \
-        $ipt -N $chain
+    local ipt="iptables -t $table"
+    $ipt -L $chain &>/dev/null && $ipt -F $chain || $ipt -N $chain
 }
 
 init_chain_pair() {
-    local chain=$1
-    init_chain filter from_$chain
-    init_chain filter to_$chain
+    local table=$1 chain=$2
+    init_chain $table from_$chain
+    init_chain $table to_$chain
 }
+
+init_chain filter ructf2014
+init_chain_pair filter inet
+for iface in $inet_ifaces; do
+    iptables -t filter -A to_inet -o $iface -j ACCEPT
+done
 
 init_network() {
     local name=${1%%:*} network=${1##*:}
-    init_chain_pair $name
-    iptables -A from_$name -s $network -j ACCEPT
-    iptables -A   to_$name -d $network -j ACCEPT
+    init_chain_pair filter $name
+    iptables -t filter -A ructf2014 -s $network -j from_$name
+    iptables -t filter -A to_$name -d $network -j ACCEPT
 }
 
-remove_chain() {
-    local table=$1 chain=$2
-    iptables -t $table -L $chain &>/dev/null && {
-        iptables -t $table -F $chain
-        iptables -t $table -X $chain
-    }
-}
-
-for net in vpn:172.16.19.0/24 \
-           core_switch:10.24.0.0/24 \
-           checksystem:10.23.0.0/24; do
-    init_network $net
+# the order of networks here is important
+# as it goes to ructf2014 chain as from_* targets in that order
+for net in vpn core_switch devs checksystem teams team_switches any; do
+    init_network $net:${!net}
 done
 
-for i in $(seq 1 $N); do
-    init_network $([ $i -eq $N ] && echo devs || echo team$i):10.23.$i.0/24
-    init_network team${i}_sw:10.24.$i.0/24
-done
+# and we fix networks here additionally in to_* chains
+# exclude core switch network from team switches
+iptables -t filter -I to_team_switches 1 -d $core_switch -j RETURN
+
+# exclude devs and checksystem networks from teams
+iptables -t filter -I to_teams 1 -d $devs -j RETURN
+iptables -t filter -I to_teams 1 -d $checksystem -j RETURN
+
+# and here we go, THE RULES
+can_go() { iptables -t filter -A from_$1 -j to_$2; }
+
+can_go vpn any
+
+can_go devs inet
+can_go devs teams
+can_go devs checksystem
+
+can_go checksystem inet
+can_go checksystem teams
+
+can_go teams inet
+$team2team && can_go teams teams
 
 #iptables -P FORWARD DROP
