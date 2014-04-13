ba9e5f56660d494dcd0bf0fc554bce2d63f648dc
Andrey Malets <andrey.malets@gmail.com>
2014-04-13 19:02:28 +0600

    persist iptables rules

--- infrastructure/router/setup.sh
+++ infrastructure/router/setup.sh
@@ -26,3 +26,10 @@ python $BASE/gen_dhcp.py $N > /etc/dhcp/dhcpd.conf
 cp $BASE/check_testimage /usr/lib/nagios/plugins
 python $BASE/gen_nagios.py $N > /etc/nagios3/conf.d/ructf2014.cfg
 /etc/init.d/nagios3 reload
+
+cp $BASE/iptables/init /etc/init.d/iptables
+cp $BASE/iptables/default /etc/default/iptables
+mkdir -p /var/lib/iptables
+
+$BASE/gen_iptables.sh $N 'eth0.300 wlan0'
+/etc/init.d/iptables save active
