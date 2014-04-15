ec7202052b6fbc9d2e2643db72d5a496edabd870
Andrew Gein <andgein@yandex.ru>
2014-04-16 01:24:47 +0600

    [meteo] error catching

--- services/meteo/data.js
+++ services/meteo/data.js
@@ -29,6 +29,8 @@ exports.get_data = function(start, callback){
 exports.add_filter = function(city, theme, callback) {
   if (! connection)
     throw 'Can\'t add filter until connection established';
+  if (theme.indexOf('http') != -1)
+    throw 'Thene can\'t contains `http` as substring';
   connection.query('INSERT INTO filters (city, theme) VALUES (?, ?)', [city, theme], function(err) {
     if (err) throw err;
     callback();
