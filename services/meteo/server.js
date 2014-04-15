0281e1f1ac64891ce4339b712d6e987f543c1d71
Andrew Gein <andgein@yandex.ru>
2014-04-16 00:59:54 +0600

    [meteo] use filters, filter by city

--- services/meteo/server.js
+++ services/meteo/server.js
@@ -3,19 +3,71 @@ var http = require("http"),
     router = require("./router"),
     data = require("./data"),
     fs = require('fs'),
-    ejs = require('ejs');
+    ejs = require('ejs'),
+    querystring = require('querystring');
 
 String.prototype.endsWith = function(suffix) {
   return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
-function index(request, response) {
+var countries = {'Russia': ['Yekaterinburg', 'Moscow', 'Saint-Petersburg']};
+
+function show_filter_index(response, filter) {
   data.get_data(0, function(rows){
-    response.writeHead(200, {"Content-type": "text/html"});
-    response.write(ejs.render(fs.readFileSync('templates/index.html', 'utf8'), {'rows': rows}));
+    data.get_filters(function(filters) {
+      response.writeHead(200, {'Content-type': 'text/html'});
+      response.write(ejs.render(fs.readFileSync('templates/index.html', 'utf8'),
+        {'rows': rows,
+         'countries': countries,
+         'filters': filters,
+         'city': filter.city
+        }));
+      response.end();
+    });
+  }); 
+}
+
+function index(request, response) {
+  show_filter_index(response, {});
+}
+
+function create_filter(request, response, post) {
+  if (post) {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    post_data = querystring.parse(post);
+    city = post_data.city;
+    theme = post_data.theme;
+    data.add_filter(city, theme, function(){
+      response.write(ejs.render(fs.readFileSync('templates/create_filter.html', 'utf8'), 
+        {'countries': countries,
+         'message': 'New filter added'
+        }));
+      response.end();
+    });
+  } else {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    response.write(ejs.render(fs.readFileSync('templates/create_filter.html', 'utf8'), 
+      {'countries': countries
+      }));
     response.end();
+  }
+}
+
+function city(request, response) {
+  var city = url.parse(request.url).pathname.split("/")
+  city = city[city.length - 1];
+  show_filter_index(response, {'city': city});
+}
+
+function filter(request, response) {
+  var filter_id = parseInt(url.parse(request.url).pathname.substr(8)) || 0;
+  console.log("Get filter: " + filter_id);
+  data.get_filter(filter_id, function(filter) {
+    response.writeHead(200, {'Content-Type': 'text/html', 'Link': filter.theme});
+    show_filter_index(response, {'city': filter.city});
   });
 }
+
 
 function get_mime_type(filename) {
   if (filename.endsWith(".css")) return "text/css";
@@ -50,16 +102,24 @@ function static_files(request, response) {
   });
 }
 
-
 router.add_route("/static/", static_files);
+router.add_route("/create_filter", create_filter);
+router.add_route("/filter/", filter);
+router.add_route("/city/", city);
 router.add_route("/", index);
 
 data.init_db(function(){
   http.createServer(function(request, response) {
     var pathname = url.parse(request.url).pathname;
+    var post_data = '';
     console.log("Request for " + pathname + " received.");
+
+    request.addListener('data', function(chunk) { post_data += chunk; });
   
-    handler = router.route(pathname);
-    handler(request, response); 
+    request.addListener('end', function(){
+      // TODO: add try-catch
+      handler = router.route(pathname);
+      handler(request, response, post_data);
+    });
   }).listen(8888);
 })
