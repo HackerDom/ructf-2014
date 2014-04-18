640ac2be3e0db18aff82c1282fe20ac241b33371
Alexander Bersenev <bay@hackerdom.ru>
2014-04-19 05:33:26 +0600

    deploy: simplify scripts a bit

--- deploy/FMGS/enter_devel_image.sh
+++ deploy/FMGS/enter_devel_image.sh
@@ -2,4 +2,4 @@
 
 service_name=FMGS
 
-docker run -it -p 44101:44100 --volumes-from ${service_name}_data --entrypoint=/bin/bash ructf2014:${service_name}_service -c /bin/bash
+docker run -it -p 44101:44100 --volumes-from ${service_name}_data ructf2014:${service_name}_service /bin/bash
--- deploy/dispatcher/enter_devel_image.sh
+++ deploy/dispatcher/enter_devel_image.sh
@@ -2,4 +2,4 @@
 
 service_name=dispatcher
 
-docker run -it -p 5001:5000 --volumes-from ${service_name}_data --entrypoint=/bin/bash ructf2014:${service_name}_service -c /bin/bash
+docker run -it -p 5001:5000 --volumes-from ${service_name}_data ructf2014:${service_name}_service /bin/bash
