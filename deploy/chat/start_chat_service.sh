c5ec81db60a5660b7982db95c1b08b878ebf373a
Alexander Bersenev <bay@hackerdom.ru>
2014-04-20 02:02:04 +0600

    deploy: add start chat service delay

--- deploy/chat/start_chat_service.sh
+++ deploy/chat/start_chat_service.sh
@@ -3,4 +3,5 @@
 service_name=chat
 
 docker start ${service_name}_mongodb
+sleep 5
 docker run --memory=500m --cpu-shares=10 --cidfile=/var/run/${service_name}_service.cont_id --link ${service_name}_mongodb:db -d -p 5555:5555 ructf2014:${service_name}_service
